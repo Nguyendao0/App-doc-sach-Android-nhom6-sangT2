4e75ec27e6d119a757ac088809410bfc9571795b
Trang <dautrang9cph2@gmail.com>
2024-03-10 22:44:38 +0700

    Commit

--- app/src/main/java/com/example/helloworldjava/MainActivity.java
+++ app/src/main/java/com/example/helloworldjava/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.helloworldjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_home = findViewById(R.id.button_home);
+        button_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
         Log.d("MainActivity" ,"Hello world");
     }
 }

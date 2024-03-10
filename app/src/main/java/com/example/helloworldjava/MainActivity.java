30d44e7e3ef5c16dd262e74d0d253d4588e8bd7a
teamweb7 <91516024+NguyEnWBIE@users.noreply.github.com>
2024-03-10 18:24:01 +0700

    TN

--- app/src/main/java/com/example/helloworldjava/MainActivity.java
+++ app/src/main/java/com/example/helloworldjava/MainActivity.java
@@ -2,15 +2,51 @@ package com.example.helloworldjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
+import android.view.View;
+
+import com.example.helloworldjava.Library.LibraryActivity;
+import com.example.helloworldjava.Library.TESTGETPDFActivity;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextToSpeech tts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity" ,"Hello world");
+
+//         tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+//            @Override
+//            public void onInit(int status) {
+//                if (status == TextToSpeech.SUCCESS) {
+//                    // Set language (US English in this case)
+//                    tts.setLanguage(Locale.US);
+//                    String text = "Hello, this is a sample text.";
+//                    tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+//
+//                } else {
+//                    Log.e("MainActivity", "TTS initialization failed");
+//                }
+//            }
+//        });
+
     }
+
+    public void GoToLibraryActivity(View view)
+    {
+        Intent intent = new Intent(this, LibraryActivity.class);
+        startActivity(intent);
+    }
+    public void goToPDF(View view)
+    {
+        Intent intent = new Intent(this, TESTGETPDFActivity.class);
+        startActivity(intent);
+    }
+
 }

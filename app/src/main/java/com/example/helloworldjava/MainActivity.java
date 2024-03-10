0c73c1ab5e9bb8703b53be113c3db563a37c87e8
LyMinh85 <mlym8565@gmail.com>
2024-03-10 23:18:47 +0700

    First commit.

--- app/src/main/java/com/example/helloworldjava/MainActivity.java
+++ app/src/main/java/com/example/helloworldjava/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.helloworldjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity" ,"Hello world");
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button btnUser = findViewById(R.id.btnUser);
+        btnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

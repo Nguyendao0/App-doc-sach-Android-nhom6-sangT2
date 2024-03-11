deb1f102801ce0200662c74da1aea6696e08ef59
Nam <ngocnam20002@gmail.com>
2024-03-11 11:39:21 +0700

    Submit

--- app/src/main/java/com/example/helloworldjava/MainActivity.java
+++ app/src/main/java/com/example/helloworldjava/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.helloworldjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity" ,"Hello world");
+        Button btnReadBook = findViewById(R.id.btbReadBook);
+        btnReadBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, ReadBookActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
+
 }

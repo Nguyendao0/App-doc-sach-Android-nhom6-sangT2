29e713c36979c47b2e65d633ba10265f85336883
Trang <dautrang9cph2@gmail.com>
2024-03-15 20:58:15 +0700

    Update.

---
+++ app/src/main/java/com/example/helloworldjava/BookCategory.java
@@ -0,0 +1,27 @@
+package com.example.helloworldjava;
+
+public class BookCategory {
+    private String title;
+    private int imageResId;
+
+    public BookCategory(String title, int imageResId) {
+        this.title = title;
+        this.imageResId = imageResId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+
+    public void setImageResId(int imageResId) {
+        this.imageResId = imageResId;
+    }
+}

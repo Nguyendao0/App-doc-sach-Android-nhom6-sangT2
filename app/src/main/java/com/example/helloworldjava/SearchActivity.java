c3802b2464827b926dc3f9a6ebb022c795d6fe33
Nguyen <clonecobth@gmail.com>
2024-03-17 22:39:30 +0700

    merge phuoc xong

--- app/src/main/java/com/example/helloworldjava/SearchActivity.java
+++ app/src/main/java/com/example/helloworldjava/SearchActivity.java
@@ -1,104 +1,24 @@
 package com.example.helloworldjava;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.*;
+import android.widget.Spinner;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import com.example.helloworldjava.GioiThieuSach.BookDetailActivity;
-import com.example.helloworldjava.Library.LibraryActivity;
-import com.example.helloworldjava.Library.TESTGETPDFActivity;
-import com.example.helloworldjava.Menu.MenuActivity;
-import com.example.helloworldjava.Thongbao.Noitification;
-
-public class MainActivity extends AppCompatActivity {
-    private TextToSpeech tts;
 public class SearchActivity extends AppCompatActivity {
-    //this is van phuoc
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d("MainActivity" ,"Hello world");
+        setContentView(R.layout.search_layout);
+        runArrayList();
 
-//         tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-//            @Override
-//            public void onInit(int status) {
-//                if (status == TextToSpeech.SUCCESS) {
-//                    // Set language (US English in this case)
-//                    tts.setLanguage(Locale.US);
-//                    String text = "Hello, this is a sample text.";
-//                    tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-//
-//                } else {
-//                    Log.e("MainActivity", "TTS initialization failed");
-//                }
-//            }
-//        });
-
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        Button btnUser = findViewById(R.id.btnUser);
-        btnUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, UserActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        // Ánh xạ button từ layout XML
-        Button openSearchButton = findViewById(R.id.openSearchButton);
-
-// Thiết lập sự kiện click cho button
-        openSearchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Xử lý khi button được click
-                // Ví dụ: mở view, thực hiện một hành động nào đó, vv...
-                runArrayList();
-            }
-        });
     }
-
-
-    public void goToMenu(View view) {
-        Intent intent = new Intent(this, MenuActivity.class);
-        startActivity(intent);
-    }
-
-    public void goToGioiThieuSach(View view) {
-        Intent intent = new Intent(this, BookDetailActivity.class);
-        startActivity(intent);
-    }
-    public void ThongBao(View view) {
-
-        Intent intent = new Intent(this, Noitification.class);
-        startActivity(intent);
-    }
-    public void GoToLibraryActivity(View view)
-    {
-        Intent intent = new Intent(this, LibraryActivity.class);
-        startActivity(intent);
-    }
-    public void goToPDF(View view)
-    {
-        Intent intent = new Intent(this, TESTGETPDFActivity.class);
-        startActivity(intent);
-    }
-
-
-
     protected void runArrayList(){
         CatergorySearch catergorySearch = new CatergorySearch();
         HistorySearch historySearch = new HistorySearch();
@@ -113,5 +33,4 @@ public class SearchActivity extends AppCompatActivity {
         AdapterListBook adapterListBook = new AdapterListBook(this, R.layout.modelbook,historySearch.getArrayBook());
         listView.setAdapter(adapterListBook);
     }
-
 }

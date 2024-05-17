cf1c73244658e165a120b0f9edf26665cba231e0
Trang <dautrang9cph2@gmail.com>
2024-05-17 17:08:01 +0700

    Update.

---
+++ app/src/main/java/com/example/helloworldjava/view/dangtruyen/ThuVienCaNhanFragment.java
@@ -0,0 +1,20 @@
+package com.example.helloworldjava.view.dangtruyen;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.helloworldjava.R;
+
+public class ThuVienCaNhanFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_thu_vien_ca_nhan, container, false);
+        return view;
+    }
+}

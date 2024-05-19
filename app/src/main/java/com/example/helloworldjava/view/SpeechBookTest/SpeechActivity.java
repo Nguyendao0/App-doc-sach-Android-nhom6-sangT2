d566ec14d2441c05aed0d3b99612ccdbd6249246
Văn Phước <vanphuochtk123456@gmail.com>
2024-05-19 23:51:18 +0700

    test change female to male
    failed

--- app/src/main/java/com/example/helloworldjava/view/SpeechBookTest/SpeechActivity.java
+++ app/src/main/java/com/example/helloworldjava/view/SpeechBookTest/SpeechActivity.java
@@ -1,17 +1,21 @@
 package com.example.helloworldjava.view.SpeechBookTest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.Voice;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.window.OnBackInvokedDispatcher;
 
 import com.example.helloworldjava.R;
 import com.example.helloworldjava.view.ReadBookActivity;
@@ -20,7 +24,9 @@ import org.w3c.dom.Text;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
 import java.util.UUID;
 
 public class SpeechActivity extends AppCompatActivity {
@@ -39,8 +45,11 @@ public class SpeechActivity extends AppCompatActivity {
         demoSpeech();
     }
 
-
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_speech,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     public void demoSpeech(){
 
@@ -60,8 +69,10 @@ public class SpeechActivity extends AppCompatActivity {
         // Tao class texttospeech, set locale tieng viet
         textToSpeech = new TextToSpeech(getApplicationContext(), status -> {
             if (status == TextToSpeech.SUCCESS) {
-                int lang = textToSpeech.setLanguage(new Locale("vie"));
-
+                int lang = textToSpeech.setLanguage(new Locale("vi", "VN"));
+                for(Voice voice : textToSpeech.getVoices()){
+                    Log.d("oninit :",voice.getName());
+                }
                 if (lang ==TextToSpeech.LANG_MISSING_DATA || lang == TextToSpeech.LANG_NOT_SUPPORTED)
                 {
                     Toast.makeText(SpeechActivity.this, "Language not supported", Toast.LENGTH_SHORT).show();
@@ -69,7 +80,7 @@ public class SpeechActivity extends AppCompatActivity {
                     Toast.makeText(SpeechActivity.this, "Language supported", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
+        },"com.google.android.tts");
         String utteranceId = UUID.randomUUID().toString();
         seekBar = (SeekBar) findViewById(R.id.seedbar_play) ;
         btnPause= (Button) findViewById(R.id.pause);
@@ -78,12 +89,20 @@ public class SpeechActivity extends AppCompatActivity {
         btnNext = (Button) findViewById(R.id.skip_next);
         btnPrevious = (Button) findViewById(R.id.skip_previous);
         btnBack = (ImageView) findViewById(R.id.btnBack);
+
+        Set<String> a=new HashSet<>();
+        a.add("male");
+       // a.add("female");
+      //  Voice v_female = new Voice("en-us-x-sfg#female_2-local",new Locale("vie"),400,200,true,a);
+        Voice v_male = new Voice("vi-vn-x-vif-network",new Locale("vi","vn"),400,200,true,null);
         // Xu li nut play
         btnPlay.setOnClickListener(v -> {
             // Toast.makeText(this, demobook.getContent(), Toast.LENGTH_SHORT).show();
            btnPlay.setOnClickListener(new View.OnClickListener() {
                @Override
                public void onClick(View v) {
+                   //textToSpeech.setVoice(v_male);
+                   textToSpeech.setVoice(new Voice("vi-vn-x-vie-network", new Locale("vi", "VN"), 400, 200, false, null));
                    textToSpeech.speak(noiDung, TextToSpeech.QUEUE_FLUSH,null,utteranceId);
                    btnPause.setVisibility(View.VISIBLE);
                    btnPlay.setVisibility(View.INVISIBLE);
@@ -112,18 +131,20 @@ public class SpeechActivity extends AppCompatActivity {
         });
         // Xu li nut Pause
         btnPause.setOnClickListener(v -> {
-                    textToSpeech.stop();
-                    btnPause.setVisibility(View.INVISIBLE);
+
+            btnPause.setVisibility(View.INVISIBLE);
             btnPlay.setVisibility(View.VISIBLE);
+            textToSpeech.stop();
         });
 
         // Xu li nut back
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                textToSpeech.stop();
                 Intent i = new Intent(SpeechActivity.this, ReadBookActivity.class);
                 SpeechActivity.this.startActivity(i);
-                textToSpeech.stop();
+
 
             }
         });
@@ -139,5 +160,6 @@ public class SpeechActivity extends AppCompatActivity {
 
         }
     }
+
 
 }
